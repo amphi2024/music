f95ddbebc2dd03e2cd19f85c98caf8c272b78986
Amphi <amphiprioninae2024@gmail.com>
2025-05-10 19:04:16 +0900

    Added size

--- lib/ui/components/repeat_icon.dart
+++ lib/ui/components/repeat_icon.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:music/models/player_service.dart';
 
 class RepeatIcon extends StatelessWidget {
-  const RepeatIcon({super.key});
+
+  final double? size;
+  const RepeatIcon({super.key, this.size});
 
   @override
   Widget build(BuildContext context) {
     return Icon(
         playerService.playMode == repeatOne ? Icons.repeat_one : Icons.repeat,
             color: playerService.playMode == playOnce ? Theme.of(context).dividerColor : null,
+      size: size,
     );
   }
 }
--- lib/ui/components/shuffle_icon.dart
+++ lib/ui/components/shuffle_icon.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:music/models/player_service.dart';
 
 class ShuffleIcon extends StatelessWidget {
-  const ShuffleIcon({super.key});
+
+  final double? size;
+  const ShuffleIcon({super.key, this.size});
 
   @override
   Widget build(BuildContext context) {
     final shuffled = playerService.shuffled;
     return Icon(
         shuffled ? Icons.shuffle : Icons.shuffle,
-        color: shuffled ? null : Theme.of(context).dividerColor
+        color: shuffled ? null : Theme.of(context).dividerColor,
+      size: size,
     );
   }
 }
