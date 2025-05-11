5a75ce191a266255936365c1dcc98262236f765d
Amphi <amphiprioninae2024@gmail.com>
2025-05-11 19:42:08 +0900

    Updated UI

--- lib/ui/components/playing/desktop_playing_bar.dart
+++ lib/ui/components/playing/desktop_playing_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:music/channels/app_method_channel.dart';
+import 'package:music/models/app_state.dart';
 import 'package:music/models/music/song.dart';
 import 'package:music/models/player_service.dart';
 import 'package:music/ui/components/album_cover.dart';
@@ -19,18 +21,17 @@ class _DesktopPlayingBarState extends State<DesktopPlayingBar> {
 
   @override
   Widget build(BuildContext context) {
-    var themeData = Theme.of(context);
     final song = widget.song;
     final screenWidth = MediaQuery.of(context).size.width;
     double controlsPanelWidth = 750;
-    if(screenWidth <= 1300) {
+    if(screenWidth <= 1450) {
       controlsPanelWidth = 350;
     }
 
     return AnimatedPositioned(
       duration: Duration(milliseconds: 1000),
         curve: Curves.easeOutQuint,
-      left: 15,
+      left: 215,
         right: 15,
         bottom: 15,
         child: AnimatedContainer(
@@ -60,14 +61,24 @@ class _DesktopPlayingBarState extends State<DesktopPlayingBar> {
                       padding: const EdgeInsets.all(8.0),
                       child: ClipRRect(borderRadius: BorderRadius.circular(10), child: AlbumCover(album: playerService.nowPlaying().album)),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(song.title.byContext(context)),
-                          Text(song.artist.name.byContext(context))
-                        ],
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                song.title.byContext(context),
+                              maxLines: 1,
+                              overflow: TextOverflow.visible,
+                            ),
+                           Text(
+                               song.artist.name.byContext(context),
+                             textAlign: TextAlign.center,
+                           )
+                          ],
+                        ),
                       ),
                     )
                   ],
@@ -75,6 +86,7 @@ class _DesktopPlayingBarState extends State<DesktopPlayingBar> {
               ),
               SizedBox(
                 width: controlsPanelWidth,
+                height: 60,
                 child: DesktopPlayControls(
                   setState: setState,
                 ),
@@ -83,32 +95,23 @@ class _DesktopPlayingBarState extends State<DesktopPlayingBar> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Icon( playerService.volume > 0.5 ? Icons.volume_up : playerService.volume > 0.1 ? Icons.volume_down : Icons.volume_mute ),
-                  Theme(
-                      data: ThemeData(
-                          sliderTheme: SliderThemeData(
-                              disabledActiveTrackColor: themeData.dividerColor,
-                              trackHeight: 3,
-                              inactiveTrackColor: themeData.dividerColor,
-                              thumbShape: RoundSliderThumbShape(enabledThumbRadius: 6.5),
-                              overlayShape: SliderComponentShape.noOverlay,
-                              activeTrackColor: themeData.dividerColor
-                          )
-                      ),
-                      child: SizedBox(
-                        width: 80,
-                        child: Slider(
-                            max: 1,
-                            value: playerService.volume,
-                            onChanged: (value) {
-                              appMethodChannel.setVolume(value);
-                              setState(() {
-                                playerService.volume = value;
-                              });
-                            }),
-                      )),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.lyrics)),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.devices)),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.list)),
+                  SizedBox(
+                    width: 80,
+                    child: Slider(
+                        max: 1,
+                        value: playerService.volume,
+                        onChanged: (value) {
+                          appMethodChannel.setVolume(value);
+                          setState(() {
+                            playerService.volume = value;
+                          });
+                        }),
+                  ),
+                  IconButton(onPressed: () {
+                    appState.setMainViewState(() {
+                      appState.floatingMenuShowing = !appState.floatingMenuShowing;
+                    });
+                  }, icon: Icon(Icons.menu)),
                 ],
               ),
             ],
