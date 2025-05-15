e3cc10bc3cde9968d43c40b0c35cef080291eda4
amphi2024 <amphiprioninae2024@gmail.com>
2025-05-15 09:54:28 +0900

    Initial commit

---
+++ lib/ui/components/add_item_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../../models/app_state.dart';
+import '../../models/app_storage.dart';
+import '../../models/music/album.dart';
+import '../../models/music/artist.dart';
+import '../dialogs/edit_album_dialog.dart';
+import '../dialogs/edit_artist_dialog.dart';
+import '../dialogs/edit_playlist_dialog.dart';
+
+class AddItemButton extends StatelessWidget {
+  const AddItemButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(icon: Icon(Icons.add_circle_outline),
+        itemBuilder: (context) {
+          return [
+            PopupMenuItem(child: Text("Song"), onTap: () async {
+              appStorage.selectMusicFilesAndSave();
+            }),
+            PopupMenuItem(
+                child: Text("Album"), onTap: () {
+              showDialog(context: context, builder: (context) {
+                return EditAlbumDialog(creating: true, album: Album.created(metadata: {}, artistId: "", albumCover: []), onSave: (album) {
+                  appState.setFragmentState(() {
+                    appStorage.albums[album.id] = album;
+                    appStorage.albumIdList.add(album.id);
+                    appStorage.albumIdList.sortAlbumList();
+                  });
+                });
+              });
+            }),
+            PopupMenuItem(
+                child: Text("Artist"), onTap: () {
+              showDialog(context: context, builder: (context) {
+                return EditArtistDialog(artist: Artist.created({}), onSave: (artist) {
+                  appState.setFragmentState(() {
+                    appStorage.artists[artist.id] = artist;
+                    appStorage.artistIdList.add(artist.id);
+                    appStorage.artistIdList.sortArtistList();
+                  });
+                });
+              });
+            }),
+            PopupMenuItem(child: Text("Playlist"), onTap: () {
+              showDialog(context: context, builder: (context) {
+                return EditPlaylistDialog(onSave: (playlist) {
+                  appState.setFragmentState(() {
+                    appStorage.playlists[playlist.id] = playlist;
+                  });
+                });
+              });
+            })
+          ];
+        });
+  }
+}
