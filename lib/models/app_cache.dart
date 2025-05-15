779f769e295cf4c5934641c2225f0fecb947edf7
amphi2024 <amphiprioninae2024@gmail.com>
2025-05-15 09:55:23 +0900

    Added play state caching

--- lib/models/app_cache.dart
+++ lib/models/app_cache.dart
@@ -5,4 +5,23 @@ final appCacheData = AppCacheData.getInstance();
 class AppCacheData extends AppCacheDataCore {
     static final AppCacheData _instance = AppCacheData();
     static AppCacheData getInstance() => _instance;
+
+    set lastPlayedPlaylistId(value) => data["lastPlayedPlaylistId"] = value;
+    String get lastPlayedPlaylistId => data["lastPlayedPlaylistId"] ?? "";
+
+    set lastPlayedSongId(String value) => data["lastPlayedSongId"] = value;
+    String get lastPlayedSongId => data["lastPlayedSongId"] ?? "";
+
+    set shuffled(bool value) => data["shuffled"] = value;
+    bool get shuffled => data["shuffled"] ?? true;
+
+    set playMode(int value) => data["playMode"] = value;
+    int get playMode => data["playMode"] ?? 0;
+
+    set uploadingFiles(value) => data["uploadingFiles"] = value;
+    List<dynamic> get uploadingFiles => data.putIfAbsent("uploadingFiles", () => []);
+
+    set volume(value) => data["volume"] = value;
+    double get volume => data["volume"] ?? 0.5;
+
 }
