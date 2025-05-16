a92209aa1a8521e6f4806eb84874a7369c4916c4
siar1234 <siar019283@gmail.com>
2025-05-16 20:43:42 +0900

    Removed unnecessary import

--- lib/ui/fragments/albums_fragment.dart
+++ lib/ui/fragments/albums_fragment.dart
@@ -1,6 +1,5 @@
 import 'package:amphi/widgets/dialogs/confirmation_dialog.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:music/ui/components/item/album_grid_item.dart';
 
