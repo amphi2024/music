15ac0e31492794aa9cce63e18547ce38122c1e66
siar1234 <siar019283@gmail.com>
2025-05-19 18:10:30 +0900

    Replace _CustomButton with shared Component

--- lib/ui/fragments/components/album_fragment_title.dart
+++ lib/ui/fragments/components/album_fragment_title.dart
@@ -5,6 +5,9 @@ import 'package:music/models/music/album.dart';
 import 'package:music/models/music/song.dart';
 import 'package:music/models/player_service.dart';
 import 'package:music/ui/components/image/album_cover.dart';
+import 'package:music/ui/fragments/components/floating_button.dart';
+
+import '../../dialogs/edit_album_dialog.dart';
 
 class AlbumFragmentTitle extends StatelessWidget {
 
@@ -23,90 +26,67 @@ class AlbumFragmentTitle extends StatelessWidget {
             child: AlbumCover(album: album),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-                  Text(album.title.byContext(context), style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
-                  )),
-                  Text(album.artist.name.byContext(context)),
-              Padding(
-                padding: const EdgeInsets.only(top: 15.0),
-                child: Row(
-                  children: [
-                    _CustomButton(icon: Icons.play_arrow, onPressed: () {
-                      if(album.songs.isNotEmpty) {
-                        appState.setState(() {
-                          var id = album.songs[0];
-                          var song = appStorage.songs.get(id);
-                          playerService.isPlaying = true;
-                          playerService.startPlay(song: song, playlistId: "!ALBUM,${album.id}");
-                          if(playerService.shuffled) {
-                            playerService.toggleShuffle();
-                          }
-                        });
-                      }
-                    }),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: _CustomButton(icon: Icons.shuffle, onPressed: () {
-                        if(album.songs.isNotEmpty) {
-                          var id = album.songs[0];
-                          var song = appStorage.songs.get(id);
-                          appState.setState(() {
-                            playerService.isPlaying = true;
-                            playerService.startPlay(song: song, playlistId: "!ALBUM,${album.id}");
-                            if(!playerService.shuffled) {
-                              playerService.toggleShuffle();
-                            }
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                    GestureDetector(
+                      onLongPress: () {
+                        showDialog(context: context, builder: (context) => EditAlbumDialog(album: album, onSave: (a) {
+                          appState.setFragmentState(() {
+
                           });
-                        }
-                      }),
+                        }));
+                      },
+                      child: Text(album.title.byContext(context), style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold
+                      )),
                     ),
-                  ],
-                ),
-              )
-            ],
+                    Text(album.artist.name.byContext(context)),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          FloatingButton(icon: Icons.play_arrow, onPressed: () {
+                            if(album.songs.isNotEmpty) {
+                              appState.setState(() {
+                                var id = album.songs[0];
+                                var song = appStorage.songs.get(id);
+                                playerService.isPlaying = true;
+                                playerService.startPlay(song: song, playlistId: "!ALBUM,${album.id}");
+                                playerService.shuffled = false;
+                              });
+                            }
+                          }),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15.0),
+                            child: FloatingButton(icon: Icons.shuffle, onPressed: () {
+                              if(album.songs.isNotEmpty) {
+                                var id = album.songs[0];
+                                var song = appStorage.songs.get(id);
+                                appState.setState(() {
+                                  playerService.isPlaying = true;
+                                  playerService.startPlay(song: song, playlistId: "!ALBUM,${album.id}", shuffle: true);
+                                });
+                              }
+                            }),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ],
     );
-  }
-}
-
-class _CustomButton extends StatelessWidget {
-
-  final IconData icon;
-  final void Function() onPressed;
-  const _CustomButton({required this.icon, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: 40.0,
-        height: 40.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Theme.of(context).shadowColor,
-              spreadRadius: 1,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: IconButton(
-            style: IconButton.styleFrom(
-                backgroundColor: Theme.of(context).floatingActionButtonTheme.backgroundColor,
-                shape: const CircleBorder()),
-            onPressed: onPressed,
-            icon: Icon(
-              color: Theme.of(context).floatingActionButtonTheme.focusColor,
-              icon,
-            )));
   }
 }
