2aae6f95960e80f8a112a319193d7ba43e556481
siar1234 <siar019283@gmail.com>
2025-05-22 19:31:44 +0900

    Update UI

--- lib/ui/dialogs/edit_lyrics_dialog.dart
+++ lib/ui/dialogs/edit_lyrics_dialog.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:music/ui/components/lyrics_editor.dart';
+
+import '../../models/lyrics_editing_controller.dart';
+import '../../models/music/lyrics.dart';
 
 class EditLyricsDialog extends StatefulWidget {
-  const EditLyricsDialog({super.key});
+  final LyricsEditingController lyricsEditingController;
+  final void Function(Lyrics) onChanged;
+  const EditLyricsDialog({super.key, required this.lyricsEditingController, required this.onChanged});
 
   @override
   State<EditLyricsDialog> createState() => _EditLyricsDialogState();
@@ -10,6 +16,41 @@ class EditLyricsDialog extends StatefulWidget {
 class _EditLyricsDialogState extends State<EditLyricsDialog> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Dialog(
+        child: SizedBox(
+          width: 550,
+          height: 500,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(onPressed: () {
+                      Navigator.pop(context);
+                    }, icon: Icon(Icons.cancel_outlined)),
+                    Row(
+                      children: [
+                        IconButton(onPressed: () async {
+                          var lyrics = await Lyrics.fromSelectedFile("default");
+                          setState(() {
+                            widget.lyricsEditingController.lyrics = lyrics;
+                          });
+                        }, icon: Icon(Icons.import_export)),
+                        IconButton(onPressed: () {
+                          widget.onChanged(widget.lyricsEditingController.lyrics);
+                          Navigator.pop(context);
+                        }, icon: Icon(Icons.check_circle_outline)),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Expanded(child: LyricsEditor(lyricsEditingController: widget.lyricsEditingController))
+            ],
+          ),
+        )
+    );
   }
 }
