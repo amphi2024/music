46c12d5691ba2f30d932744156e6e642aff6c094
siar1234 <siar019283@gmail.com>
2025-05-22 10:15:35 +0900

    Improve UI performance

--- lib/ui/fragments/archive_fragment.dart
+++ lib/ui/fragments/archive_fragment.dart
@@ -4,6 +4,7 @@ import 'package:music/models/app_storage.dart';
 import 'package:music/ui/components/item/song_list_item.dart';
 
 import '../components/image/album_cover.dart';
+import 'components/fragment_padding.dart';
 
 class ArchiveFragment extends StatefulWidget {
   const ArchiveFragment({super.key});
@@ -45,34 +46,28 @@ class _ArchiveFragmentState extends State<ArchiveFragment> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> children = [];
-    children.add(Container(
-      height: 50,
-    ));
-    for (int i = 0; i < appStorage.archiveIdList.length; i++) {
-      final id = appStorage.archiveIdList[i];
-      var song = appStorage.songs.get(id);
-      var albumCover = Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        child: SizedBox(
+    return ListView.builder(
+      padding: fragmentPadding(context),
+      controller: scrollController,
+      itemCount: appStorage.archiveIdList.length,
+      itemBuilder: (context, index) {
+
+        final id = appStorage.archiveIdList[index];
+        var song = appStorage.songs.get(id);
+        var albumCover = Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: SizedBox(
             width: 50,
             height: 50,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
-              child: AlbumCover(
-                album: song.album,
-              ),
-            )
-        ),
-      );
-      children.add(SongListItem(song: song, playlistId: "", albumCover: albumCover));
-    }
-    children.add(Container(
-      height: 80,
-    ));
-    return ListView(
-      controller: scrollController,
-      children: children,
+              child: AlbumCover(album: song.album),
+            ),
+          ),
+        );
+        return SongListItem(song: song, playlistId: "!ARCHIVE", albumCover: albumCover);
+
+      },
     );
   }
 }
