2eb215fa31b932ad6545ebc7b8b3cc95a8fc72ab
Siar <siar019283@gmail.com>
2025-06-30 23:19:04 +0900

    fix: temporarily prevent runtime error by commenting out

--- linux/runner/metadata_retriever.cc
+++ linux/runner/metadata_retriever.cc
@@ -4,20 +4,20 @@ std::map<std::string, std::string> MusicMetadata(std::string filePath)
 {
 
   std::map<std::string, std::string> data = {};
-
-  TagLib::FileRef file(filePath.c_str());
-  if (!file.isNull() && file.tag()) {
-
-      TagLib::Tag* tag = file.tag();
-
-      data["title"]       = tag->title().toCString();
-      data["artist"]      = tag->artist().toCString();
-      data["album"]       = tag->album().toCString();
-      data["comment"]     = tag->comment().toCString();
-      data["genre"]       = tag->genre().toCString();
-      data["year"]        = std::to_string(tag->year());
-      data["trackNumber"] = std::to_string(tag->track());
-  }
+//
+//  TagLib::FileRef file(filePath.c_str());
+//  if (!file.isNull() && file.tag()) {
+//
+//      TagLib::Tag* tag = file.tag();
+//
+//      data["title"]       = tag->title().toCString();
+//      data["artist"]      = tag->artist().toCString();
+//      data["album"]       = tag->album().toCString();
+//      data["comment"]     = tag->comment().toCString();
+//      data["genre"]       = tag->genre().toCString();
+//      data["year"]        = std::to_string(tag->year());
+//      data["trackNumber"] = std::to_string(tag->track());
+//  }
 
   return data;
 }
