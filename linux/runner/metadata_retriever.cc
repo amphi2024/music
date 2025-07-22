0f60214f3bc6a2ff5e32cebfc20664794b819173
Siar <siar019283@gmail.com>
2025-07-22 21:33:26 +0900

    ci: upgrade Ubuntu to 24.04 to fix mpv dependency errors

--- linux/runner/metadata_retriever.cc
+++ linux/runner/metadata_retriever.cc
@@ -5,19 +5,19 @@ std::map<std::string, std::string> MusicMetadata(std::string filePath)
 
   std::map<std::string, std::string> data = {};
 
-  TagLib::FileRef file(filePath.c_str());
-  if (!file.isNull() && file.tag()) {
-
-      TagLib::Tag* tag = file.tag();
-
-      data["title"]       = tag->title().to8Bit(true);
-      data["artist"]      = tag->artist().to8Bit(true);
-      data["album"]       = tag->album().to8Bit(true);
-      data["comment"]     = tag->comment().to8Bit(true);
-      data["genre"]       = tag->genre().to8Bit(true);
-      data["year"]        = std::to_string(tag->year());
-      data["trackNumber"] = std::to_string(tag->track());
-  }
+//  TagLib::FileRef file(filePath.c_str());
+//  if (!file.isNull() && file.tag()) {
+//
+//      TagLib::Tag* tag = file.tag();
+//
+//      data["title"]       = tag->title().to8Bit(true);
+//      data["artist"]      = tag->artist().to8Bit(true);
+//      data["album"]       = tag->album().to8Bit(true);
+//      data["comment"]     = tag->comment().to8Bit(true);
+//      data["genre"]       = tag->genre().to8Bit(true);
+//      data["year"]        = std::to_string(tag->year());
+//      data["trackNumber"] = std::to_string(tag->track());
+//  }
 
   return data;
 }
