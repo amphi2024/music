2d2427fca8a23e56042a023d671c05ed3d6b5c09
siar1234 <siar019283@gmail.com>
2025-12-19 16:52:56 +0900

    chore: apply padding only on mobile

--- lib/ui/fragments/components/fragment_padding.dart
+++ lib/ui/fragments/components/fragment_padding.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/widgets.dart';
+import 'package:music/utils/screen_size.dart';
 
 EdgeInsets fragmentPadding(BuildContext context) {
+  if(isDesktopOrTablet(context)) {
+    return EdgeInsets.zero;
+  }
   final padding = MediaQuery.paddingOf(context);
-  return EdgeInsets.zero;
+  return EdgeInsets.only(top: 55 + padding.top, bottom: 100 + padding.bottom);
 }
