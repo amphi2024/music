a600b304a3b6457421e9d59f9c4388a680fdd5ae
siar1234 <siar019283@gmail.com>
2025-12-19 16:12:43 +0900

    fix: download album cover when not available on device

--- lib/ui/components/image/album_cover.dart
+++ lib/ui/components/image/album_cover.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:music/channels/app_web_channel.dart';
+import 'package:music/models/app_settings.dart';
 import 'package:music/models/music/album.dart';
 import 'package:music/utils/media_file_path.dart';
 
@@ -12,9 +14,20 @@ class AlbumCover extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cover = album.covers.firstOrNull;
+    final cover = album.covers.elementAtOrNull(album.coverIndex ?? 0);
     if(cover != null) {
-      return AbsoluteAlbumCover(filePath: albumCoverPath(album.id, cover["filename"]), fit: fit);
+      return AbsoluteAlbumCover(filePath: albumCoverPath(album.id, cover["filename"]), fit: fit,
+          errorBuilder: (context, error, stackTrace) {
+            if(appSettings.useOwnServer) {
+              appWebChannel.downloadAlbumCover(albumId: album.id, filename: cover["filename"]);
+              return Image(image: NetworkImage("${appSettings.serverAddress}/music/albums/${album.id}/covers/${cover["filename"]}", headers: {
+                "Authorization": appWebChannel.token
+              }), errorBuilder: (context, error, stackTrace) {
+                return Image.asset("assets/images/music.png", fit: fit);
+              });
+            }
+            return Image.asset("assets/images/music.png", fit: fit);
+          });
     }
     else {
       return Image.asset("assets/images/music.png", fit: fit);
@@ -26,7 +39,8 @@ class AbsoluteAlbumCover extends StatelessWidget {
 
   final String filePath;
   final BoxFit? fit;
-  const AbsoluteAlbumCover({super.key, required this.filePath, this.fit});
+  final ImageErrorWidgetBuilder? errorBuilder;
+  const AbsoluteAlbumCover({super.key, required this.filePath, this.fit, this.errorBuilder});
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +48,7 @@ class AbsoluteAlbumCover extends StatelessWidget {
     return Image.file(
       file,
       fit: fit,
+      errorBuilder: errorBuilder,
     );
   }
 }
