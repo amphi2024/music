8876c067ec80b3bd0acf003c94f0742210ce80ad
siar1234 <siar019283@gmail.com>
2025-12-19 16:47:34 +0900

    feat: add fetching of songs, albums, artists, and playlists on refresh

--- lib/utils/data_sync.dart
+++ lib/utils/data_sync.dart
@@ -1,5 +1,6 @@
 import 'package:amphi/models/update_event.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:music/database/database_helper.dart';
 import 'package:music/models/connected_device.dart';
 import 'package:music/providers/albums_provider.dart';
 import 'package:music/providers/artists_provider.dart';
@@ -9,26 +10,57 @@ import 'package:music/providers/songs_provider.dart';
 import '../channels/app_web_channel.dart';
 import '../models/app_storage.dart';
 
-void refreshDataWithServer(WidgetRef ref) {
-  // appWebChannel.getNotes(onSuccess: (list) async {
-  //   for (var item in list) {
-  //     final id = item["id"];
-  //     if (id is String) {
-  //       final database = await databaseHelper.database;
-  //       final List<Map<String, dynamic>> noteList = await database.rawQuery("SELECT * FROM notes WHERE id = ?", [id]);
-  //       if(noteList.isEmpty) {
-  //         await appWebChannel.downloadNote(id: id, onSuccess: (note) {
-  //           note.save(upload: false);
-  //           if(!note.isFolder) {
-  //             note.initTitles();
-  //           }
-  //           ref.read(notesProvider.notifier).applyServerUpdate(note);
-  //         });
-  //       }
-  //     }
-  //   }
-  // });
-  //
+void refreshDataWithServer(WidgetRef ref) async {
+  final database = await databaseHelper.database;
+  appWebChannel.getArtists(onSuccess: (list) async {
+    for(var id in list) {
+      final ids = await database.rawQuery("SELECT id FROM artists WHERE id = ?", [id]);
+      if(ids.isEmpty) {
+        appWebChannel.downloadArtist(id: id, onSuccess: (artist) {
+          artist.save(upload: false);
+          ref.read(artistsProvider.notifier).insertArtist(artist);
+          ref.read(playlistsProvider.notifier).notifyArtistUpdate(artist);
+        });
+      }
+    }
+  });
+  appWebChannel.getAlbums(onSuccess: (list) async {
+    for(var id in list) {
+      final ids = await database.rawQuery("SELECT id FROM albums WHERE id = ?", [id]);
+      if(ids.isEmpty) {
+        appWebChannel.downloadAlbum(id: id, onSuccess: (album) {
+          album.save(upload: false);
+          ref.read(albumsProvider.notifier).insertAlbum(album);
+          ref.read(playlistsProvider.notifier).notifyAlbumUpdate(album);
+        });
+      }
+    }
+  });
+  appWebChannel.getSongs(onSuccess: (list) async {
+    for(var id in list) {
+      final ids = await database.rawQuery("SELECT id FROM songs WHERE id = ?", [id]);
+      if(ids.isEmpty) {
+        appWebChannel.downloadSong(id: id, onSuccess: (song) {
+          song.save(upload: false);
+          ref.read(songsProvider.notifier).insertSong(song);
+          ref.read(playlistsProvider.notifier).notifySongUpdate(song);
+        });
+      }
+    }
+  });
+  appWebChannel.getPlaylists(onSuccess: (list) async {
+    for(var item in list) {
+      final id = item["id"];
+      final ids = await database.rawQuery("SELECT id FROM playlists WHERE id = ?", [id]);
+      if(ids.isEmpty) {
+        appWebChannel.downloadPlaylist(id: id, onSuccess: (playlist) {
+          playlist.save(upload: false);
+          ref.read(playlistsProvider.notifier).insertPlaylist(playlist);
+        });
+      }
+    }
+  });
+
   // appWebChannel.getThemes(onSuccess: (list) async {
   //   for (var item in list) {
   //     final id = item["id"];
