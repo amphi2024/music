6f9ec237485b7fceda9e1e0fbf6db554266c47dc
siar1234 <siar019283@gmail.com>
2025-12-27 11:46:52 +0900

    feat: add trash fragment

---
+++ lib/ui/fragments/trash_fragment.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:music/providers/albums_provider.dart';
+import 'package:music/providers/artists_provider.dart';
+import 'package:music/utils/localized_title.dart';
+
+import '../../providers/playlists_provider.dart';
+import '../../providers/songs_provider.dart';
+import '../../utils/fragment_scroll_listener.dart';
+import 'components/fragment_padding.dart';
+
+class TrashFragment extends ConsumerStatefulWidget {
+  const TrashFragment({super.key});
+
+  @override
+  TrashFragmentState createState() => TrashFragmentState();
+}
+
+class TrashFragmentState extends ConsumerState<TrashFragment> with FragmentViewMixin {
+
+  //TODO: optimize performance, improve UI/UX
+  @override
+  Widget build(BuildContext context) {
+    final trash = ref
+        .watch(playlistsProvider)
+        .trash;
+    final songs = ref.watch(songsProvider);
+    final artists = ref.watch(artistsProvider);
+    final albums = ref.watch(albumsProvider);
+    final playlists = ref.watch(playlistsProvider).playlists;
+
+    List<Widget> children = [];
+
+    for(var id in trash.songs) {
+      final song = songs.get(id);
+      children.add(_Item(title: song.title.toLocalized(), onRestore: () {
+        song.deleted = null;
+        song.save();
+        ref.read(songsProvider.notifier).insertSong(song);
+        ref.read(playlistsProvider.notifier).notifySongUpdate(song);
+      }, onDelete: () {
+        song.delete();
+        ref.read(songsProvider.notifier).removeSong(song.id);
+        ref.read(playlistsProvider.notifier).deleteSong(song.id);
+      }));
+    }
+
+    for(var id in trash.albums) {
+      final album = albums.get(id);
+      children.add(_Item(title: album.title.toLocalized(), onRestore: () {
+        album.deleted = null;
+        album.save();
+        ref.read(albumsProvider.notifier).insertAlbum(album);
+        ref.read(playlistsProvider.notifier).notifyAlbumUpdate(album);
+      }, onDelete: () {
+        album.delete();
+        ref.read(albumsProvider.notifier).removeAlbum(album.id);
+        ref.read(playlistsProvider.notifier).deleteAlbum(album.id);
+      }));
+    }
+
+    for(var id in trash.artists) {
+      final artist = artists.get(id);
+      children.add(_Item(title: artist.name.toLocalized(), onRestore: () {
+        artist.deleted = null;
+        artist.save();
+        ref.read(artistsProvider.notifier).insertArtist(artist);
+        ref.read(playlistsProvider.notifier).notifyArtistUpdate(artist);
+      }, onDelete: () {
+        artist.delete();
+        ref.read(artistsProvider.notifier).removeArtist(artist.id);
+        ref.read(playlistsProvider.notifier).deleteArtist(artist.id);
+      }));
+    }
+
+    for(var id in trash.playlists) {
+      final playlist = playlists.get(id);
+      children.add(_Item(title: playlist.title, onRestore: () {
+        playlist.deleted = null;
+        playlist.save();
+        ref.read(playlistsProvider.notifier).insertPlaylist(playlist);
+      }, onDelete: () {
+        playlist.delete();
+        ref.read(playlistsProvider.notifier).deletePlaylist(playlist.id);
+      }));
+    }
+
+    return ListView(
+      padding: fragmentPadding(context),
+      controller: scrollController,
+      children: children,
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+
+  final String title;
+  final void Function() onRestore;
+  final void Function() onDelete;
+  const _Item({required this.title, required this.onRestore, required this.onDelete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          PopupMenuButton(
+              icon: Icon(Icons.more_vert),
+              tooltip: "",
+              itemBuilder: (context) {
+                return [
+                  //TODO: localize
+                  PopupMenuItem(child: Text("restore"), onTap: onRestore),
+                  PopupMenuItem(child: Text("delete"), onTap: onDelete),
+                ];
+              })
+        ],
+      ),
+    );
+  }
+}
