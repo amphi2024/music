6e0bed8f12534a71e7a0fde23ca8773c9819e623
siar1234 <siar019283@gmail.com>
2026-01-02 14:09:51 +0900

    chore: bump version code to 3
    - Rebuilt AAB due to issue discovered after uploading to Google Play

--- lib/ui/components/image/artist_profile_image.dart
+++ lib/ui/components/image/artist_profile_image.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:music/models/music/artist.dart';
 import 'package:music/utils/media_file_path.dart';
 
+import '../../../channels/app_web_channel.dart';
+import '../../../models/app_settings.dart';
+
 class ArtistProfileImage extends StatelessWidget {
 
   final Artist artist;
@@ -13,7 +16,18 @@ class ArtistProfileImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(artist.images.isNotEmpty) {
-      return AbsoluteArtistProfileImage(filePath: artistImagePath(artist.id, artist.images[artist.imageIndex]["filename"]));
+      return AbsoluteArtistProfileImage(
+          filePath: artistImagePath(artist.id, artist.images[artist.imageIndex]["filename"]), fit: fit,           errorBuilder: (context, error, stackTrace) {
+        if(appSettings.useOwnServer) {
+          appWebChannel.downloadArtistImage(artistId: artist.id, filename: artist.images[artist.imageIndex]["filename"]);
+          return Image(image: NetworkImage("${appSettings.serverAddress}/music/artists/${artist.id}/images/${artist.images[artist.imageIndex]["filename"]}", headers: {
+            "Authorization": appWebChannel.token
+          }), errorBuilder: (context, error, stackTrace) {
+            return Image.asset("assets/images/music.png", fit: fit);
+          });
+        }
+        return Image.asset("assets/images/music.png", fit: fit);
+      });
     }
     else {
       return Image.asset("assets/images/music.png", fit: fit);
@@ -25,17 +39,16 @@ class AbsoluteArtistProfileImage extends StatelessWidget {
 
   final String filePath;
   final BoxFit? fit;
-  const AbsoluteArtistProfileImage({super.key, required this.filePath, this.fit = BoxFit.cover});
+  final ImageErrorWidgetBuilder? errorBuilder;
+  const AbsoluteArtistProfileImage({super.key, required this.filePath, this.fit = BoxFit.cover, this.errorBuilder});
 
   @override
   Widget build(BuildContext context) {
       var file = File(filePath);
-      if(!file.existsSync()) {
-        return Image.asset("assets/images/music.png", fit: fit);
-      }
       return Image.file(
         file,
         fit: fit,
+        errorBuilder: errorBuilder,
       );
     }
 }
